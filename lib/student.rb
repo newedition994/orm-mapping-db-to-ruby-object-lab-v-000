d7df01bc979302a9e4a3b853a7452fc4723dd322
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-27 20:34:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,11 @@ class Student
   end
 
   def self.students_below_12th_grade
-    
+    sql = <<-SQL
+    SELECT COUNT(*) FROM students
+    WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql)
   end
 
 end
