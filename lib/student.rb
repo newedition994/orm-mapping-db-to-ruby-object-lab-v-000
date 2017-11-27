8fdc011412813223cd0294e4defd4052b905a697
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-27 20:30:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,13 @@ class Student
   end
 
   def self.find_by_name(name)
-    
+    sql = <<-SQL
+    SELECT * FROM students 
+    WHERE name = ? LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def save
